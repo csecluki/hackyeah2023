dadf933262ff4b3ed92fb3e0a9f1ceeed46e8aa9
csecluki <lukaszzagorski99@gmail.com>
2023-09-30 15:17:19 +0200

    Add basic models

--- models/PowerStation.js
+++ models/PowerStation.js
@@ -1,3 +1,48 @@
 class PowerStation {
 
+    constructor(builldingCost, powerProduction, pollution) {
+        this.builldingCost = builldingCost
+        this.powerProduction = powerProduction
+        this.pollution = pollution
+    }
+}
+
+class AtomicPowerStation extends PowerStation {
+    
+    constructor(builldingCost, powerProduction, pollution) {
+        super(builldingCost, powerProduction, pollution)
+    }
+
+}
+
+class WindPowerStation extends PowerStation {
+    
+    constructor(builldingCost, powerProduction, pollution) {
+        super(builldingCost, powerProduction, pollution)
+    }
+
+}
+
+class WaterPowerStation extends PowerStation {
+    
+    constructor(builldingCost, powerProduction, pollution) {
+        super(builldingCost, powerProduction, pollution)
+    }
+
+}
+
+class CoalPowerStation extends PowerStation {
+    
+    constructor(builldingCost, powerProduction, pollution) {
+        super(builldingCost, powerProduction, pollution)
+    }
+
+}
+
+class SunPowerStation extends PowerStation {
+    
+    constructor(builldingCost, powerProduction, pollution) {
+        super(builldingCost, powerProduction, pollution)
+    }
+
 }
---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(400, 400);
+    new AtomicPowerStation(1,2,3)
+  }
+  
+  function draw() {
+    background(220);
+  } 
