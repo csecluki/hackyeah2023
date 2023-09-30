5e6508fd65d1f026ed029064bac2b7323a5e9fc2
Marcin Purzycki <marcin.purzycki@blebox.eu>
2023-10-01 00:22:56 +0200

    feat: basic control layout v2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var svg, path, canvas;
 
+var divMainCointainer, divControlPanelLeft, divControlPanelRight, divMapPanel;
 var divCountryStats, divCuntryOperations, divEuropeStats, divLogo, divMapFilters, divMapLegend;
-var divCountryStats_countryName;
+var divCountryStats_countryName, divCountryStats_countryDemand, divCountryStats_countryProduction, divCountryStats_countryPollution, divCountryStats_countryOverallCost;
+var divCountryStats_europeDemand, divCountryStats_europeProduction, divCountryStats_europePollution, divCountryStats_europeOverallCost;
 
 function preload() {
     const buildCost = loadJSON("./models/data/buildCost.json")
@@ -17,9 +19,91 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(600, 500, SVG);
-    canvas.position(250, 10);
-    image(svg, 0, 0, 600, 500);
+
+    // ======================================================
+
+    divMainCointainer = createDiv().class('mainContainer');
+    divControlPanelLeft = createDiv().class('controlPanel');
+    divMapPanel = createDiv().class('mapPanel');
+    divControlPanelRight = createDiv().class('controlPanel');
+
+    divMainCointainer.child(divControlPanelLeft);
+    divMainCointainer.child(divMapPanel);
+    divMainCointainer.child(divControlPanelRight);
+
+    // ======================================================
+
+    divLogo = createDiv().class('controlContainer');
+    divControlPanelLeft.child(divLogo);
+    divLogo.child(createDiv('Euro Power Plant<br>Simulator').class('logo'));
+
+    divMapFilters = createDiv().class('controlContainer');
+    divControlPanelLeft.child(divMapFilters);
+    divMapFilters.child(createDiv('Map Filters').class('controlHeader'));
+
+    divMapLegend = createDiv().class('controlContainer');
+    divControlPanelLeft.child(divMapLegend);
+    divMapLegend.child(createDiv('Map Legend').class('controlHeader'));
+
+    divEuropeStats = createDiv().class('controlContainer');
+    divControlPanelLeft.child(divEuropeStats);
+    divEuropeStats.child(createDiv('Europe Stats').class('controlHeader'));
+
+    divEuropeStats.child(createDiv('Energy Demand:').class('controlLabel'));
+    divCountryStats_europeDemand = createDiv('-').class('controlValue');
+    divEuropeStats.child(divCountryStats_europeDemand);
+
+    divEuropeStats.child(createDiv('Energy Production:').class('controlLabel'));
+    divCountryStats_europeProduction = createDiv('-').class('controlValue');
+    divEuropeStats.child(divCountryStats_europeProduction);
+
+    divEuropeStats.child(createDiv('Pollution:').class('controlLabel'));
+    divCountryStats_europePollution = createDiv('-').class('controlValue');
+    divEuropeStats.child(divCountryStats_europePollution);
+
+    divEuropeStats.child(createDiv('Overall Cost:').class('controlLabel'));
+    divCountryStats_europeOverallCost = createDiv('-').class('controlValue');
+    divEuropeStats.child(divCountryStats_europeOverallCost);
+
+
+    // ======================================================
+
+    divCountryStats = createDiv().class('controlContainer');
+    divControlPanelRight.child(divCountryStats);
+    divCountryStats.child(createDiv('Country Stats').class('controlHeader'));
+
+    divCountryStats.child(createDiv('Country Name:').class('controlLabel'));
+    divCountryStats_countryName = createDiv('-').class('controlValue');
+    divCountryStats.child(divCountryStats_countryName);
+
+    divCountryStats.child(createDiv('Energy Demand:').class('controlLabel'));
+    divCountryStats_countryDemand = createDiv('-').class('controlValue');
+    divCountryStats.child(divCountryStats_countryDemand);
+
+    divCountryStats.child(createDiv('Energy Production:').class('controlLabel'));
+    divCountryStats_countryProduction = createDiv('-').class('controlValue');
+    divCountryStats.child(divCountryStats_countryProduction);
+
+    divCountryStats.child(createDiv('Pollution:').class('controlLabel'));
+    divCountryStats_countryPollution = createDiv('-').class('controlValue');
+    divCountryStats.child(divCountryStats_countryPollution);
+
+    divCountryStats.child(createDiv('Overall Cost:').class('controlLabel'));
+    divCountryStats_countryOverallCost = createDiv('-').class('controlValue');
+    divCountryStats.child(divCountryStats_countryOverallCost);
+
+    divCuntryOperations = createDiv().class('controlContainer');
+    divControlPanelRight.child(divCuntryOperations);
+    divCuntryOperations.child(createDiv('Country Operations').class('controlHeader'));
+
+    // ======================================================
+
+    const canvasWidth = 600;
+    const canvasHeight = 500;
+
+    canvas = createCanvas(canvasWidth, canvasHeight, SVG);
+    canvas.parent(divMapPanel);
+    image(svg, 0, 0, canvasWidth, canvasHeight);
 
     path = querySVG('path');
     for (let i = 0; i < path.length; i++) {
@@ -27,45 +111,8 @@ function setup() {
             path[i].attribute('onclick', 'countryOnClick( path[' + i + '])');
             // set attribute random color
             path[i].attribute('fill', color(random(255), random(255), random(255)));
-
-
-
         }
     }
-
-    divCountryStats = createDiv('Country Stats');
-    divCountryStats.style('font-weight', 'bold');
-    divCountryStats.position(860, 10);
-    divCountryStats.child(createDiv('Country Name:'));
-    divCountryStats_countryName = createDiv('none');
-
-    divCountryStats.child(divCountryStats_countryName);
-
-    divCountryStats.child(createDiv('Energy Demand:'));
-    divCountryStats.child(createDiv('Energy Production:'));
-    divCountryStats.child(createDiv('Pollution:'));
-    divCountryStats.child(createDiv('Overall Cost:'));
-
-    divCuntryOperations = createDiv('Country Operations');
-    divCuntryOperations.style('font-size', '16px');
-    divCuntryOperations.position(860, 400);
-
-    divEuropeStats = createDiv('Europe Stats');
-    divEuropeStats.style('font-size', '16px');
-    divEuropeStats.position(10, 400);
-
-    divLogo = createDiv('Euro Power Plant<br>Simulator');
-    divLogo.style('font-size', '16px');
-    divLogo.position(10, 10);
-
-    divMapFilters = createDiv('Map Filters');
-    divMapFilters.style('font-size', '16px');
-    divMapFilters.position(10, 70);
-
-    divMapLegend = createDiv('Map Legend');
-    divMapLegend.style('font-size', '16px');
-    divMapLegend.position(10, 200);
-
 }
 
   
