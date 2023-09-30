8f680b450992cfe782f58ded8511dd34d96014ed
Marcin Purzycki <marcin.purzycki@blebox.eu>
2023-09-30 21:14:45 +0200

    feat: basic control layout

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
-var svg, path;
+var svg, path, canvas;
+
+var divCountryStats, divCuntryOperations, divEuropeStats, divLogo, divMapFilters, divMapLegend;
+var divCountryStats_countryName;
 
 function preload() {
     const buildCost = loadJSON("./models/data/buildCost.json")
@@ -14,22 +17,68 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(600, 500, SVG);
+    canvas = createCanvas(600, 500, SVG);
+    canvas.position(250, 10);
     image(svg, 0, 0, 600, 500);
-    
+
     path = querySVG('path');
     for (let i = 0; i < path.length; i++) {
         if (path[i].id() !== "") {
             path[i].attribute('onclick', 'countryOnClick( path[' + i + '])');
+            // set attribute random color
+            path[i].attribute('fill', color(random(255), random(255), random(255)));
+
+
+
         }
     }
+
+    divCountryStats = createDiv('Country Stats');
+    divCountryStats.style('font-weight', 'bold');
+    divCountryStats.position(860, 10);
+    divCountryStats.child(createDiv('Country Name:'));
+    divCountryStats_countryName = createDiv('none');
+
+    divCountryStats.child(divCountryStats_countryName);
+
+    divCountryStats.child(createDiv('Energy Demand:'));
+    divCountryStats.child(createDiv('Energy Production:'));
+    divCountryStats.child(createDiv('Pollution:'));
+    divCountryStats.child(createDiv('Overall Cost:'));
+
+    divCuntryOperations = createDiv('Country Operations');
+    divCuntryOperations.style('font-size', '16px');
+    divCuntryOperations.position(860, 400);
+
+    divEuropeStats = createDiv('Europe Stats');
+    divEuropeStats.style('font-size', '16px');
+    divEuropeStats.position(10, 400);
+
+    divLogo = createDiv('Euro Power Plant<br>Simulator');
+    divLogo.style('font-size', '16px');
+    divLogo.position(10, 10);
+
+    divMapFilters = createDiv('Map Filters');
+    divMapFilters.style('font-size', '16px');
+    divMapFilters.position(10, 70);
+
+    divMapLegend = createDiv('Map Legend');
+    divMapLegend.style('font-size', '16px');
+    divMapLegend.position(10, 200);
+
 }
+
   
 function draw() {
 
 }
 
 function countryOnClick(pathOfCountry) {
-    pathOfCountry.attribute('fill', 'red');
-    console.log(pathOfCountry.id());
+    divCountryStats_countryName.html(pathOfCountry.id());
+    for (let i = 0; i < path.length; i++) {
+        if (path[i].id() !== "") {
+            path[i].attribute('opacity', 1);
+        }
+    }
+    pathOfCountry.attribute('opacity', 0.5);
 }
